8883fe16d3eff812ea86cc67ad9186c256e1453c
Radosław Czapliński <radekk2604@gmail.com>
2023-11-29 15:27:41 +0100

    Funkcjonalność 3

--- PetrolPricesAPI/routes/web.php
+++ PetrolPricesAPI/routes/web.php
@@ -4,5 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('app');
-})
+});
+    Route::get('/fuel-stations', 'FuelStationController@index')->name('fuel-stations.index');
+    Route::post('/fuel-stations/search', 'FuelStationController@search')->name('fuel-stations.search')
+
 ->name('application');
