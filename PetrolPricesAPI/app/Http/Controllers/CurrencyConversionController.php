6364ac81225ab3978b11e422cd0ab864c411a5c8
filosekm <marceli.filosek@studenci.collegiumwitelona.pl>
2023-12-26 18:53:57 +0100

    fixed CurrencyConversionController

--- PetrolPricesAPI/app/Http/Controllers/CurrencyConversionController.php
+++ PetrolPricesAPI/app/Http/Controllers/CurrencyConversionController.php
@@ -2,22 +2,28 @@
 
 namespace App\Http\Controllers;
 
-public function convert(Request $request)
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class CurrencyConversionController extends Controller
 {
-    $fromCurrency = $request->query('from');
-    $toCurrency = $request->query('to');
-    $amount = $request->query('amount');
+    public function convert(Request $request)
+    {
+        $fromCurrency = $request->query('from');
+        $toCurrency = $request->query('to');
+        $amount = $request->query('amount');
 
-    $apiKey = env('EXCHANGE_RATE_API_KEY');
-    $response = Http::get("https://api.exchangerate-api.com/v4/latest/{$fromCurrency}?apiKey={$apiKey}");
+        $apiKey = env('EXCHANGE_RATE_API_KEY');
+        $response = Http::get("https://api.exchangerate-api.com/v4/latest/{$fromCurrency}?apiKey={$apiKey}");
 
-    if ($response->successful()) {
-        $rate = $response->json()['rates'][$toCurrency] ?? null;
-        if ($rate) {
-            $convertedAmount = $amount * $rate;
-            return response()->json(['convertedAmount' => $convertedAmount]);
+        if ($response->successful()) {
+            $rate = $response->json()['rates'][$toCurrency] ?? null;
+            if ($rate) {
+                $convertedAmount = $amount * $rate;
+                return response()->json(['convertedAmount' => $convertedAmount]);
+            }
         }
-    }
 
-    return response()->json(['error' => 'Nie można przeprowadzić konwersji waluty.'], 500);
+        return response()->json(['error' => 'Nie można przeprowadzić konwersji waluty.'], 500);
+    }
 }
